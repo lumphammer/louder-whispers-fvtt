673ada7ef37e2f59b5ace41325ce9d4f254ca809
Neil de Carteret <git@lumphammer.net>
2025-10-20 10:34:03 +0100

    explicit type annotations for eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from "eslint/config";
 
 import sharedConfig from "@lumphammer/shared-fvtt-bits/dotfiles/import/eslint.core.config.js";
+/** @type {import('eslint').Linter.Config[]} */
+const config = defineConfig([sharedConfig]);
 
-export default defineConfig([sharedConfig]);
+export default config;
